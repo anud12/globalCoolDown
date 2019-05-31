b46e785144eaad1b26172dd0755fc90eaf9451ae
andu@andu.com <andu@andu.com>
2019-06-01 02:01:44 +0300

    commit

--- src/main/java/ro/anud/globalCooldown/engine/GameLoop.java
+++ src/main/java/ro/anud/globalCooldown/engine/GameLoop.java
@@ -50,7 +50,7 @@ public class GameLoop {
         this.worldService = Objects.requireNonNull(worldService, "worldService must not be null");
     }
 
-    @Scheduled(fixedDelayString = "${ro.anud.global-cooldown.properties.deltaTime}")
+    @Scheduled(fixedRateString = "${ro.anud.global-cooldown.properties.deltaTime}")
     private void gameLoop() {
         List<GameObjectModel> gameObjectModels = gameObjectRepository.getAll();
         HashMap<Object, List<Runnable>> commandPlanList = gameObjectModels
