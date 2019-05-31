cf682df0c6a153938c40f72bafe38b4f57155fde
andu@andu.com <andu@andu.com>
2019-05-31 20:59:20 +0300

    commit

--- src/main/java/ro/anud/globalCooldown/engine/command/planner/Command.java
+++
@@ -1,9 +0,0 @@
-package ro.anud.globalCooldown.engine.command.planner;
-
-import ro.anud.globalCooldown.data.model.GameObjectModel;
-import ro.anud.globalCooldown.engine.command.CommandScope;
-
-public interface Command {
-    CommandPlan plan(final GameObjectModel gameObjectModel,
-                     final CommandScope commandScope);
-}
--- src/main/java/ro/anud/globalCooldown/engine/command/planner/CommandValidator.java
+++
@@ -1,9 +0,0 @@
-package ro.anud.globalCooldown.engine.command.planner;
-
-import ro.anud.globalCooldown.data.model.GameObjectModel;
-import ro.anud.globalCooldown.engine.command.CommandScope;
-
-public interface CommandValidator {
-    boolean validate(final GameObjectModel gameObjectModel,
-                                               final CommandScope commandScope);
-}
